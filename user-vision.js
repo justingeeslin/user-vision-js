213ce52edc387c1728fcec2c18e97f7c85156642
Justin Geeslin <justin@geesl.in>
2022-12-28 22:44:01 -0600

    Following best practices using Beacon API

--- user-vision.js
+++ user-vision.js
@@ -2,24 +2,13 @@ function log(e) {
     console.log('Click', e);
 }
 
-function save() {
-    console.log('Saving session..');
-
-    var url = "save.php";
-    var params = "lorem=ipsum&name=alpha";
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", url, true);
-
-    //Send the proper header information along with the request
-    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-
-    xhr.send(params);
-}
   
 document.body.addEventListener("click", log);
 
-window.addEventListener("unload", save);
-
-// window.onunload = function() {
-//     save();
-// }
+document.addEventListener('visibilitychange', function logData() {
+    if (document.visibilityState === 'hidden') {
+        console.log('Saving session..');
+        analyticsData = {};
+        navigator.sendBeacon('/save.php', analyticsData);
+    }
+});
