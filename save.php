c7381c5a1d6eaefb202e09f4ac55f8c05d751804
Justin Geeslin <justin@geesl.in>
2022-12-28 22:43:28 -0600

    Saving script

--- save.php
+++ save.php
@@ -1,4 +1,24 @@
 <?php
-echo "Saved!";
+header('Access-Control-Allow-Origin: *');
+// header('Access-Control-Allow-Headers:  Content-Type, X-Auth-Token, Authorization, Origin');
+// header('Access-Control-Allow-Methods:  POST, PUT, GET');
+
+$GLOBALS['resultsFile'] = "results.csv";
+
+// Sanitize input
+$_POST  = filter_input_array(INPUT_POST, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+
+function recordResults() {
+    echo "Recording results… "; 
+    $myfile = fopen($GLOBALS['resultsFile'], "a") or die("Unable to open file!");
+    $data = "Testing\n";
+    fwrite($myfile, $data);
+    echo "Done!";
+    fclose($myfile);
+}
+
+recordResults();
+
+echo "Saved2!";
 
 ?>
